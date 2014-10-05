b944dded86c06cd83c8173b67de80e8e8ac33407
danigrant <thedanigrant@gmail.com>
2014-10-04 21:35:00 -0400

    calling api from master branch site

--- CONTRIBUTING.md
+++
@@ -1,3 +0,0 @@
-### Contributing to shaaaaaaaaaaaaa
-
-By contributing to this project, you agree to license your contributions under the [MIT License](LICENSE).
--- app.js
+++ app.js
@@ -1,38 +1,30 @@
-// this is an Express app
-var express = require('express');
-var app = express();
+window.onload = function(){
+	//format string
+	var currURL = window.location.host;
+	var url = currURL.split("www.")[1];
+	console.log(url);
 
-// environment and port
-var env = process.env.NODE_ENV || 'development';
-var port = parseInt(process.argv[2], 10);
-if (isNaN(port)) port = 3000;
-
-// app middleware/settings
-app.engine('.html', require('ejs').__express);
-app.enable('trust proxy')
-  .use(require('body-parser').json())
-  .use(require('body-parser').urlencoded({extended: false}))
-  .use(require('method-override')())
-  .use(function(req,res,next){
-    res.locals.req = req;
-    next();
-  })
-  .use(express.static(__dirname + '/public'));
-
-// development vs production
-if (env == "development")
-  app.use(require('errorhandler')({dumpExceptions: true, showStack: true}))
-else
-  app.use(require('errorhandler')())
-
-app.locals.helpers = {
-  escape_attribute: function(text) {
-    return text.replace(/\"/g, "&quot;");
-  }
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", ("https://shaaaaaaaaaaaaa.com/api/check/" + url), true);
+	xhr.onreadystatechange = function() {
+	  if (xhr.readyState == 4) {
+	    var resp = JSON.parse(xhr.responseText);
+	    var diagnosis = resp.diagnosis;
+	    switch (diagnosis) {
+	    	case "good":
+	    		chrome.browserAction.setIcon({path: 'good.png'});
+	    		console.log("good");
+	    		break;
+	    	case "almost":
+	    		chrome.browserAction.setIcon({path: 'almost.png'});
+	    		console.log("almost");
+		    	break;
+	    	case "bad":
+	    		chrome.browserAction.setIcon({path: 'bad.png'});
+	    		console.log("oof");
+		    	break;
+	    }
+	  }
+	}
+	xhr.send();
 };
-var routes = require("./routes")(app);
-
-// boot it up!
-app.listen(port, function() {
-  console.log("Express server listening on port %s in %s mode", port, env);
-});
